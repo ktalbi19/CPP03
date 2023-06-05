ca3e11ed07b706a88fb1c6aefef9f01bbd8d0b00
Auriane Gillet <103597382+AurianeGillet@users.noreply.github.com>
2023-06-05 12:21:59 +0200

    CPP03

---
+++ ex02/main.cpp
@@ -0,0 +1,13 @@
+#include "FragTrap.hpp"
+
+int main(void)
+{
+    FragTrap ash("Ash");
+
+    ash.attack("the air");
+    ash.takeDamage(10);
+    ash.beRepaired(10);
+    ash.highFive();
+
+    return (0);
+}
